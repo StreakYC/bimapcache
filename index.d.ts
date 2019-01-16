ac96d3f5e78966e740988186f2f0fd1a679ec414
Chris Cowan <agentme49@gmail.com>
2019-01-15 18:50:53 -0800

    Add TypeScript type defs

---
+++ index.d.ts
@@ -0,0 +1,18 @@
+export type Options<A extends string|number=string,B extends string|number=string> = {
+  key: string;
+  getAfromB(b: B): Promise<A>;
+  getBfromA(a: A): Promise<B>;
+  storage?: Storage;
+  saveThrottle?: number;
+  maxLimit?: number;
+  maxAge?: number;
+};
+
+// The type parameters are mainly so Flow helps us avoid mixing up A and B.
+// Generally you'll probably have them both be strings.
+export default class BiMapCache<A extends string|number=string,B extends string|number=string> {
+  constructor(options: Options<A,B>);
+
+  getBfromA(a: A): Promise<B>;
+  getAfromB(b: B): Promise<A>;
+}
